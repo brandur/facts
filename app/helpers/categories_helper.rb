6f942cb55515579ec6a6472baa41bd784c049ddb
fyrerise <fyrerise@mutelight.org>
2010-09-24 16:18:16 -0600

    * modularizing object count string

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,11 @@
 module CategoriesHelper
+  def count_str(category)
+    if category.children.count > 0 && category.facts.count > 0
+      "#{pluralize(category.facts.count, 'fact')}, #{pluralize(category.children.count, 'category')}"
+    elsif category.facts.count > 0
+      "#{pluralize(category.facts.count, 'fact')}"
+    elsif category.children.count > 0
+      "#{pluralize(category.children.count, 'category')}"
+    end
+  end
 end
